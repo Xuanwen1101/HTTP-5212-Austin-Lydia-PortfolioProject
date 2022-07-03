44c4bd8183071c821388ea853d44b97c26c48a67
AC59852 <austin.caron1@gmail.com>
2022-07-03 09:59:53 -0400

    adding original DB connections back

--- admin/users_add.php
+++ admin/users_add.php
@@ -17,13 +17,13 @@ if( isset( $_POST['first'] ) )
         last,
         email,
         password,
-        status
+        active
       ) VALUES (
         "'.mysqli_real_escape_string( $connect, $_POST['first'] ).'",
         "'.mysqli_real_escape_string( $connect, $_POST['last'] ).'",
         "'.mysqli_real_escape_string( $connect, $_POST['email'] ).'",
         "'.md5( $_POST['password'] ).'",
-        "'.$_POST['status'].'"
+        "'.$_POST['active'].'"
       )';
     mysqli_query( $connect, $query );
     

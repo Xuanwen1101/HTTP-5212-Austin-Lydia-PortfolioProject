5919204e764f068b9d5e823ed5182530d2541892
AC59852 <austin.caron1@gmail.com>
2022-07-03 07:23:16 -0400

    testing

--- admin/contact.php
+++ admin/contact.php
@@ -9,31 +9,12 @@
   $message = $_POST['message'];
 
 
-  // log the email
-  echo $email;
-
-  // get the user from the database with the email of "austin.caron1@gmail.com"
-  $query = 'SELECT * FROM users WHERE email = austin.caron1@gmail.com"';
-  $result = mysqli_query( $connect, $query );
-
-  // if there is a user with the email
-  if( mysqli_num_rows( $result ) )
-  {
-    $record = mysqli_fetch_assoc( $result );
-    
-    // send an email to the email inserted in the form
-    $to = $email;
-    $subject = 'Test Email';
-    $message = $name . '<br> Your message: ' . $message;
-    $headers = 'From: ' . $record['email'];
-
-    $headers .= "MIME-Version: 1.0\n";
-    $headers .= "Content-type: text/html; charset=us-ascii\n"; 
-
-    mail( $to, $subject, $message, $headers );
-
-  }
-  else
-  {
-    echo 'No user found';
-  }
+  ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    $from = "emailtest@YOURDOMAIN";
+    $to = "austin.caron1@gmail.com";
+    $subject = "PHP Mail Test script";
+    $message = "This is a test to check the PHP Mail functionality";
+    $headers = "From:" . $from;
+    mail($to,$subject,$message, $headers);
+    echo "Test email sent";
